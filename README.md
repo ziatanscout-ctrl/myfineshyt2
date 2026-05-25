37ca1db1d5eacd1bb21dcbd0f45a17bb0d275c72
ZIATAN IBRAHIMOVIC <ziatanscout@gmail.com>
2026-05-26 00:52:46 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,10 @@
+
+## Run Locally
+
+**Prerequisites:**  Node.js
+
+
+1. Install dependencies:
+   `npm install`
+3. Run the app:
+   `npm run dev`
---
+++ vite.config.ts
@@ -0,0 +1,31 @@
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import {defineConfig, loadEnv} from 'vite';
+import { viteSingleFile } from "vite-plugin-singlefile";
+
+export default defineConfig(({mode}) => {
+  const env = loadEnv(mode, '.', '');
+  return {
+    plugins: [react(), tailwindcss(), viteSingleFile()],
+    define: {
+      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, '.'),
+      },
+    },
+    build: {
+      cssCodeSplit: false,
+      assetsInlineLimit: 100000000,
+    },
+    server: {
+      // HMR is disabled in AI Studio via DISABLE_HMR env var.
+      // Do not modifyâfile watching is disabled to prevent flickering during agent edits.
+      hmr: process.env.DISABLE_HMR !== 'true',
+      // Disable file watching when DISABLE_HMR is true to save CPU during agent edits.
+      watch: process.env.DISABLE_HMR === 'true' ? null : {},
+    },
+  };
+});
